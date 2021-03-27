8c221bd0e3aafbf12b45295cd3f4013ba0aa5aa1
unknown <a.guna14@gmail.com>
2021-03-27 22:20:38 +0530

    added fabric js

--- storyboard-ui/src/main/webapp/app/home/home.component.ts
+++ storyboard-ui/src/main/webapp/app/home/home.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/auth/account.model';
+import { fabric } from 'fabric';
 
 @Component({
   selector: 'jhi-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
   account: Account | null = null;
   authSubscription?: Subscription;
+  @ViewChild('htmlCanvas') htmlCanvas: ElementRef;
+  public size: any = {
+    width: 800,
+    height: 500,
+  };
+  private canvas: fabric.Canvas;
 
   constructor(private accountService: AccountService, private router: Router) {}
 
@@ -32,5 +39,21 @@ export class HomeComponent implements OnInit, OnDestroy {
     if (this.authSubscription) {
       this.authSubscription.unsubscribe();
     }
+  }
+
+  ngAfterViewInit(): void {
+    this.canvas = new fabric.Canvas(this.htmlCanvas.nativeElement, {
+      hoverCursor: 'pointer',
+      selection: true,
+      selectionBorderColor: 'blue',
+    });
+
+    this.canvas.setWidth(this.size.width);
+    this.canvas.setHeight(this.size.height);
+    fabric.Image.fromURL('../../content/images/jhipster_family_member_0_head-192.png', aImg => {
+      // Scale down our image size
+      // aImg.scale(0.5);
+      this.canvas.add(aImg);
+    });
   }
 }
